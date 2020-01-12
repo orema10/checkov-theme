210f1b8d7c2e4a2f526c5b166df63c9b18f68312
mgmt1pyro <59607004+mgmt1pyro@users.noreply.github.com>
2020-01-12 11:51:17 +0200

    add dropdown effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 console.log('Script Loaded!');
+
+var elDropdowns = document.querySelectorAll('nav .dropdown');
+if(elDropdowns.length > 0) {
+    elDropdowns.forEach(function(dropdown) {
+        dropdown.querySelector('.dropdown-title').addEventListener('click', function(e) {
+            e.preventDefault();
+            var elDropdownContent = dropdown.querySelector('.dropdown-content');
+            if(dropdown.classList.contains('open')) {
+                elDropdownContent.style.height = 0 + 'px';
+                dropdown.classList.remove('open');
+            } else {
+                var currHeight = elDropdownContent.querySelector('.dropdown-content-container').offsetHeight;
+                elDropdownContent.style.height = currHeight + 'px';
+                dropdown.classList.add('open');
+            }
+        })
+    })
+}
