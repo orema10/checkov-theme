f73e15b4b2d71d23986f916fe7d07c3b855018b7
mgmt1pyro <59607004+mgmt1pyro@users.noreply.github.com>
2020-01-12 12:09:36 +0200

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ console.log('Script Loaded!');
 var elDropdowns = document.querySelectorAll('nav .dropdown');
 if(elDropdowns.length > 0) {
     elDropdowns.forEach(function(dropdown) {
+        var elDropdownContent = dropdown.querySelector('.dropdown-content');
         if(dropdown.querySelector('.current')) {
-            dropdown.style.height = 'auto';
+            elDropdownContent.style.height = 'auto';
             dropdown.classList.add('open');
         }
         dropdown.querySelector('.dropdown-title').addEventListener('click', function(e) {
             e.preventDefault();
-            var elDropdownContent = dropdown.querySelector('.dropdown-content');
             if(dropdown.classList.contains('open')) {
                 elDropdownContent.style.height = 0 + 'px';
                 dropdown.classList.remove('open');
